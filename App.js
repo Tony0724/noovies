60826d4ca10ecb5e47530ffb6c0db12ff291acd5
Tony0724 <0724ksm@naver.com>
2023-05-30 21:41:39 +0900

    made stack navigation and configuring

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {Ionicons} from '@expo/vector-icons'
 import {Text} from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from './navigation/Tabs';
+import Stack from './navigation/Stack';
 
 export default function App() {
 	const [loaded] = Font.useFonts(Ionicons.font)
@@ -13,7 +14,8 @@ export default function App() {
 	}
 	return (
 		<NavigationContainer>
-			<Tabs />
+			{/* <Tabs /> */}
+			<Stack />
 		</NavigationContainer>
 	);
 }
