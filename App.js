0ac4b71a3d66764e416f6626254e83b06a4c744f
Tony0724 <0724ksm@naver.com>
2023-06-08 17:17:59 +0900

    made Themes and install typescript

--- App.js
+++ App.js
@@ -2,19 +2,25 @@ import AppLoading from 'expo-app-loading'
 import React, {useState} from 'react'
 import * as Font from 'expo-font'
 import {Ionicons} from '@expo/vector-icons'
-import {Text} from 'react-native';
+import {useColorScheme} from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Root from './navigation/Root';
+import { darkTheme, lightTheme } from './styled';
+import { ThemeProvider } from 'styled-components/native'
 
 
 export default function App() {
-	const [loaded] = Font.useFonts(Ionicons.font)
+	const [loaded] = Font.useFonts(Ionicons.font);
+	const isDark = useColorScheme() === 'dark';
+
 	if(!loaded) {
 		return <AppLoading />
 	}
 	return (
-		<NavigationContainer>
-			<Root />
-		</NavigationContainer>
+		<ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+			<NavigationContainer>
+				<Root />
+			</NavigationContainer>
+		</ThemeProvider>
 	);
 }
