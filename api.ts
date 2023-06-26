19142389339cca3201e8969071a6f5554a654c38
Tony0724 <0724ksm@naver.com>
2023-06-26 16:02:00 +0900

    fix typescript type error

--- api.ts
+++ api.ts
@@ -1,6 +1,33 @@
 const API_KEY = '8001745014d67322d3711a9e484dbd40';
 const BASE_URL = "https://api.themoviedb.org/3";
 
+export interface Movie {
+    adult: boolean;
+    backdrop_path: string | null;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+interface BaseResponse {
+    page: number;
+    total_results: number;
+    total_pages: number;
+}
+
+export interface MovieResponse extends BaseResponse {
+    results: Movie[]
+}
+
 const Trending = () => fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then(res => res.json());
 
 const UpComing = () => fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`).then(res => res.json());
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -7,7 +7,7 @@ import Slide from "../components/Slide"
 import VMedia from '../components/VMedia';
 import HMedia from '../components/HMedia';
 import {useQuery, useQueryClient} from 'react-query'
-import { moviesApi } from '../api';
+import { MovieResponse, moviesApi } from '../api';
 
 const Loader = styled.View`
 	flex: 1;
@@ -45,45 +45,28 @@ const HSeperator = styled.View`
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">>= () => {
 	const queryClient = useQueryClient()
-	const {isLoading: nowPlayingLoading, data: nowPlayingData, isRefetching: isRefetchingNowPlaying} = useQuery(["movies", "nowPlaying"], moviesApi.nowPlaying)
-	const {isLoading: upcomingLoading, data: upcomingData, isRefetching: isRefetchingUpcoming} = useQuery(["movies", "upcoming"], moviesApi.UpComing)
-	const {isLoading: trendingLoading, data: trendingData, isRefetching: isRefetchingTrending} = useQuery(["movies", "trending"], moviesApi.Trending)
+	const {isLoading: nowPlayingLoading, data: nowPlayingData, isRefetching: isRefetchingNowPlaying} = useQuery<MovieResponse>(["movies", "nowPlaying"], moviesApi.nowPlaying)
+	const {isLoading: upcomingLoading, data: upcomingData, isRefetching: isRefetchingUpcoming} = useQuery<MovieResponse>(["movies", "upcoming"], moviesApi.UpComing)
+	const {isLoading: trendingLoading, data: trendingData, isRefetching: isRefetchingTrending} = useQuery<MovieResponse>(["movies", "trending"], moviesApi.Trending)
 	const onRefresh = async() => {
 		queryClient.refetchQueries(["movies"])
 	}
-	const renderVMedia = ({item}) => (
-		<VMedia
-      posterPath={item.poster_path}
-      originalTitle={item.original_title}
-      voteAverage={item.vote_average}
-    />
-	)
-	const renderHMedia = ({item}) => (
-		<HMedia
-      posterPath={item.poster_path}
-      originalTitle={item.original_title}
-      overview={item.overview}
-      releaseDate={item.release_date}
-    />
-	)
-	const movieKeyExtractor = (item) => item.id + "";
 	const loading = nowPlayingLoading || upcomingLoading || trendingLoading;
 	const refreshing = isRefetchingNowPlaying || isRefetchingUpcoming || isRefetchingTrending
 	return loading ? 
 	<Loader>
 		<ActivityIndicator  />
 	</Loader>
-	 : (
-			<FlatList
+	 : ( upcomingData ? <FlatList
 				onRefresh={onRefresh}
 				refreshing={refreshing} 
 				ListHeaderComponent={
 					<>
 					<Swiper horizontal loop autoplay autoplayTimeout={3.5} showsButtons={false} showsPagination={false} containerStyle={{width: '100%', height: SCREEN_HEIGHT / 3}} style={{marginBottom: 30}}>
-					{nowPlayingData.results.map(movie => <Slide 
+					{nowPlayingData?.results.map(movie => <Slide 
 						key={movie.id} 
-						backdrop_path={movie.backdrop_path }
-						poster_path={movie.poster_path } 
+						backdrop_path={movie.backdrop_path || "" }
+						poster_path={movie.poster_path || "" } 
 						original_title={movie.original_title } 
 						vote_average={movie.vote_average } 
 						overview={movie.overview}
@@ -91,23 +74,36 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">>= () => {
 				</Swiper>
 				<ListContainer>
 					<ListTitle>Trending Movies</ListTitle>
-					<TrendingScroll
+					{trendingData ? <TrendingScroll
 						data={trendingData.results}
 						contentContainerStyle={{paddingHorizontal: 20}}
 						horizontal 
 						showsHorizontalScrollIndicator={false}
 						ItemSeparatorComponent={VSeperator}
-						keyExtractor={movieKeyExtractor}
-						renderItem={renderVMedia}
-					/>
+						keyExtractor={(item) => item.id + ""}
+						renderItem={({item}) => (
+							<VMedia
+						  posterPath={item.poster_path}
+						  originalTitle={item.original_title}
+						  voteAverage={item.vote_average}
+						/>
+						)}
+					/>: null}
 					</ListContainer>
 					<ComingSoonTitle>Coming soon</ComingSoonTitle>
 				</>}
-				data={upcomingData.results}
-				keyExtractor={movieKeyExtractor}
+				data={upcomingData?.results}
+				keyExtractor={(item) => item.id + ""}
 				ItemSeparatorComponent={HSeperator}
-				renderItem={renderHMedia}
-			/>
+				renderItem={({item}) => (
+					<HMedia
+				  posterPath={item.poster_path}
+				  originalTitle={item.original_title}
+				  overview={item.overview}
+				  releaseDate={item.release_date}
+				/>
+				)}
+			/> : null
 	 	)
 }
 
