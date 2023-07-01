95964f5db8832550da00239ef1ba79f1c37fb68f
Tony0724 <0724ksm@naver.com>
2023-07-01 13:34:07 +0900

    made search fetcher

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { useColorScheme } from 'react-native';
+import { moviesApi, tvApi } from '../api';
+import { useQuery } from 'react-query';
 
 const Container = styled.ScrollView``;
 
 const Search = () => {
 	const isDark = useColorScheme() === 'dark';
+	const [query, setQuery] = useState("");
+	const {isLoading: moviesLoading, data: moviesData, refetch: searchMovies} = useQuery(["searchMovies", query], moviesApi.search);
+	const {isLoading: tvLoading, data: tvData, refetch: searchTv} = useQuery(["searchTv", query], tvApi.search)
 	const SearchBar = styled.TextInput`
 		background-color: ${isDark ? "white" : "black"};
 		padding: 10px 15px;
@@ -13,15 +18,26 @@ const Search = () => {
 		width: 90%;
 		margin: 10px auto;
 	`;
-	const [query, setQuery] = useState("");
-	const onChangeText = (text:string) => setQuery(text);
+	let Text = "";	
+	const onSubmit = () => {
+		setQuery(Text);
+		if(query === "") {
+			return;
+		}
+		searchMovies();
+		searchTv();
+	}
 	return (
 		<Container>
 			<SearchBar
 				placeholder='Search for Movie or Tv Show' 
 				placeholderTextColor={isDark ? "grey" : "#FFFFFFcc"} 
 				returnKeyType='search'
-				onChangeText={onChangeText}	
+				autoCapitalize='none'
+				onSubmitEditing={onSubmit}
+				onChangeText={(text:string) => {
+					Text = text;
+				}}	
 			/>
 		</Container>
 	)
